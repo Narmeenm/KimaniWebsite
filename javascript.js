8ade584d081235aec69b2c5b0157c93f25506366
Narmeen Massalha <narmeenmassalha4975@gmail.com>
2021-02-10 00:59:41 +0200

    finishing all the popup of the experince

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ function myFunction() {
 var popup = document.getElementById("myPopup");
 popup.classList.toggle("show");
 }
+
 function second() {
 var popup = document.getElementById("myPopup2");
 popup.classList.toggle("show");
@@ -9,5 +10,10 @@ popup.classList.toggle("show");
 
 function third() {
 var popup = document.getElementById("myPopup3");
+popup.classList.toggle("show");
+}
+
+function fourth() {
+var popup = document.getElementById("myPopup4");
 popup.classList.toggle("show");
 }
