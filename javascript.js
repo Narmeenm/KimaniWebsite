441facd570ef6cd2e386dad8045d77095b6306d9
Narmeen Massalha <narmeenmassalha4975@gmail.com>
2021-02-10 00:35:48 +0200

    I created three of popup

--- javascript.js
+++ javascript.js
@@ -1,21 +1,13 @@
-var barChart = document.getElementById('barChart').getContext('2d');
-var myBarChart = new Chart(barChart, {
-    type: 'bar',
-    data: {
-      labels: ["Leadership", "Goal-Oriented", "Creativity", "Criticial Thinking", "Design Thinking","Digital Business Development"],
-      datasets: [
-        {
-          label: "skills (types)",
-          backgroundColor: ["#8D6440", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#8D6440"],
-          data: [8,7,6,5,8,9]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'My Skills'
-      }
-    }
-});
+function myFunction() {
+var popup = document.getElementById("myPopup");
+popup.classList.toggle("show");
+}
+function second() {
+var popup = document.getElementById("myPopup2");
+popup.classList.toggle("show");
+}
+
+function third() {
+var popup = document.getElementById("myPopup3");
+popup.classList.toggle("show");
+}
